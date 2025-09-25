734c9f2719dac933298350df1c67676e6fd2c743
FATECHANGE2024 <rajm43439@gmail.com>
2025-09-26 00:11:56 +0530

    5th time

--- next.config.ts
+++ next.config.ts
@@ -3,20 +3,12 @@ const nextConfig = {
   images: {
     domains: ['cfafdwbhkrjzzmpqmmmo.supabase.co'], // allow this domain
   },
-  api: {
+   api: {
     bodyParser: {
       sizeLimit: '100mb',
     },
   },
-  webpack: (config: any) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      stream: require.resolve("stream-browserify"),
-      crypto: require.resolve("crypto-browserify"),
-      buffer: require.resolve("buffer"),
-    };
-    return config;
-  },
 };
 
 module.exports = nextConfig;
+
